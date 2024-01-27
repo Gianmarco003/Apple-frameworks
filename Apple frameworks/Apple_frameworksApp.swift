9c32e29cb2d68b9e47a18585b8990426754bcc3b
Gianmarco <gimmy03@icloud.com>
2024-01-27 10:25:15 +0100

    Initial Commit

---
+++ Apple frameworks/Apple_frameworksApp.swift
@@ -0,0 +1,17 @@
+//
+//  Apple_frameworksApp.swift
+//  Apple frameworks
+//
+//  Created by Gianmarco on 27/01/24.
+//
+
+import SwiftUI
+
+@main
+struct Apple_frameworksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
